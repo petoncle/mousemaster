56f180ef74912f398c5138d3877689fe73195450
petoncle <albert.petoncle47@gmail.com>
2023-11-05 00:00:00 +0100

    Add mouse event hook

--- src/main/java/jmouseable/jmouseable/JmouseableApplication.java
+++ src/main/java/jmouseable/jmouseable/JmouseableApplication.java
@@ -22,42 +22,54 @@ public class JmouseableApplication implements CommandLineRunner {
         SpringApplication.run(JmouseableApplication.class, args);
     }
 
-    private WinUser.HHOOK keyboardHookHandle;
+    private WinUser.HHOOK keyboardHook;
+    private WinUser.HHOOK mouseHook;
 
     @Override
     public void run(String... args) throws Exception {
         WinDef.HMODULE hMod = Kernel32.INSTANCE.GetModuleHandle(null);
-        WinUser.LowLevelKeyboardProc keyboardHook = new WinUser.LowLevelKeyboardProc() {
-            public WinDef.LRESULT callback(int nCode, WinDef.WPARAM wParam,
-                                           WinUser.KBDLLHOOKSTRUCT info) {
-                if (nCode >= 0) {
-                    switch (wParam.intValue()) {
-                        case WinUser.WM_KEYUP:
-                        case WinUser.WM_KEYDOWN:
-                        case WinUser.WM_SYSKEYUP:
-                        case WinUser.WM_SYSKEYDOWN:
-                            logger.info("In callback, key state: " + wParam + ", " +
-                                        info.vkCode);
-                            // If you want to stop the event from continuing you would do so here
-                            break;
-                    }
-                }
-                return User32.INSTANCE.CallNextHookEx(keyboardHookHandle, nCode, wParam,
-                        new WinDef.LPARAM(Pointer.nativeValue(info.getPointer())));
-            }
-        };
-        keyboardHookHandle =
-                User32.INSTANCE.SetWindowsHookEx(WinUser.WH_KEYBOARD_LL, keyboardHook,
-                        hMod, 0);
+        keyboardHook = User32.INSTANCE.SetWindowsHookEx(WinUser.WH_KEYBOARD_LL,
+                (WinUser.LowLevelKeyboardProc) this::keyboardHookCallback, hMod, 0);
+        mouseHook = User32.INSTANCE.SetWindowsHookEx(WinUser.WH_MOUSE_LL,
+                (WinUser.LowLevelMouseProc) this::mouseHookCallback, hMod, 0);
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             logger.info("Running shutdown hook");
-            User32.INSTANCE.UnhookWindowsHookEx(keyboardHookHandle);
+            User32.INSTANCE.UnhookWindowsHookEx(keyboardHook);
+            User32.INSTANCE.UnhookWindowsHookEx(mouseHook);
         }));
-        logger.info("Keyboard hook installed");
+        logger.info("Keyboard and mouse hooks installed");
         WinUser.MSG msg = new WinUser.MSG();
         while (User32.INSTANCE.GetMessage(msg, null, 0, 0) != 0) {
             User32.INSTANCE.TranslateMessage(msg);
             User32.INSTANCE.DispatchMessage(msg);
         }
     }
+
+    private WinDef.LRESULT keyboardHookCallback(int nCode, WinDef.WPARAM wParam,
+                                                WinUser.KBDLLHOOKSTRUCT info) {
+        if (nCode >= 0) {
+            switch (wParam.intValue()) {
+                case WinUser.WM_KEYUP:
+                case WinUser.WM_KEYDOWN:
+                case WinUser.WM_SYSKEYUP:
+                case WinUser.WM_SYSKEYDOWN:
+                    logger.info("In callback, key state: " + wParam + ", " + info.vkCode);
+                    // If you want to stop the event from continuing you would do so here
+                    break;
+            }
+        }
+        return User32.INSTANCE.CallNextHookEx(keyboardHook, nCode, wParam,
+                new WinDef.LPARAM(Pointer.nativeValue(info.getPointer())));
+    }
+
+    private WinDef.LRESULT mouseHookCallback(int nCode, WinDef.WPARAM wParam,
+                                             WinUser.MSLLHOOKSTRUCT info) {
+        if (nCode >= 0) {
+            WinDef.POINT p = info.pt;
+            System.out.println("Mouse position: (" + p.x + "," + p.y + ")");
+        }
+        return User32.INSTANCE.CallNextHookEx(mouseHook, nCode, wParam,
+                new WinDef.LPARAM(Pointer.nativeValue(info.getPointer())));
+    }
+
 }
