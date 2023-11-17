7898d6da4c5096245dc162a2398077c85ca9ea81
petoncle <albert.petoncle47@gmail.com>
2023-11-18 00:00:00 +0100

    Start introducing graalvm native image

--- src/main/java/jmouseable/jmouseable/JmouseableApplication.java
+++ src/main/java/jmouseable/jmouseable/JmouseableApplication.java
@@ -1,9 +1,14 @@
 package jmouseable.jmouseable;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.nio.file.Paths;
 
 public class JmouseableApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(JmouseableApplication.class);
 
     public static void main(String[] args) throws InterruptedException, IOException {
         Configuration configuration =
@@ -21,6 +26,17 @@ public class JmouseableApplication {
                 new IndicatorManager(modeManager, mouseManager, keyboardManager);
         Ticker ticker = new Ticker(modeManager, mouseManager, keyboardManager,
                 indicatorManager);
+        if (args.length == 1 && args[0].equals("-graalvm-agent-run")) {
+            logger.info("-graalvm-agent-run flag found, exiting in 20s");
+            new Thread(() -> {
+                try {
+                    Thread.sleep(20000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.exit(0);
+            }).start();
+        }
         new WindowsHook(mouseManager, keyboardManager, ticker).installHooks();
     }
 
