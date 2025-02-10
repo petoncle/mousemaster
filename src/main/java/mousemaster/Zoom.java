f4ab5e3ea8a7d3a099a1e0bde172d1adb3e5f05c
petoncle <albert.petoncle47@gmail.com>
2025-02-11 00:00:00 +0100

    Fix calculations of zoomed position

--- src/main/java/mousemaster/Zoom.java
+++ src/main/java/mousemaster/Zoom.java
@@ -3,19 +3,19 @@ package mousemaster;
 public record Zoom(double percent, Point center, Rectangle screenRectangle) {
 
     public double zoomedX(double x) {
-        return screenRectangle.width() / 2d + (x - center.x()) * percent;
+        return screenRectangle.center().x() + (x - center.x()) * percent;
     }
 
     public double zoomedY(double y) {
-        return screenRectangle.height() / 2d + (y - center.y()) * percent;
+        return screenRectangle.center().y() + (y - center.y()) * percent;
     }
 
     public double unzoomedX(double zoomedX) {
-        return (zoomedX - screenRectangle.width() / 2d) / percent + center.x();
+        return (zoomedX - screenRectangle.center().x()) / percent + center.x();
     }
 
     public double unzoomedY(double zoomedY) {
-        return (zoomedY - screenRectangle.height() / 2d) / percent + center.y();
+        return (zoomedY - screenRectangle.center().y()) / percent + center.y();
     }
 
 }
