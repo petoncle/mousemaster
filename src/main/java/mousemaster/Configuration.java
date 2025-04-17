fc8030b267368580e5f101f52b6e4c953ed0ace0
petoncle <albert.petoncle47@gmail.com>
2025-04-18 00:00:00 +0200

    Add hide-console property to be able to hide the mousemaster window

--- src/main/java/mousemaster/Configuration.java
+++ src/main/java/mousemaster/Configuration.java
@@ -2,7 +2,7 @@ package mousemaster;
 
 public record Configuration(int maxPositionHistorySize, ModeMap modeMap, String logLevel,
                             boolean logRedactKeys, boolean logToFile,
-                            KeyboardLayout configurationKeyboardLayout) {
+                            boolean hideConsole, KeyboardLayout configurationKeyboardLayout) {
 
 
 }
