f69fa9398df3eea1de7c87bd9179d74b5fa386e8
petoncle <albert.petoncle47@gmail.com>
2025-09-13 00:00:00 +0200

    Add runtime warning when hint.select is incorrectly configured
    In neomousekeys-ijkl, enter hint mode then press a hint key while holding leftshift: this will (incorrectly) switch to hint2-mode (and trigger a NPE because lastSelectedHintPoint is null) if hint.select is:
    hint1-mode.hint.select=+extendedhint1key
    instead of:
    hint1-mode.hint.select=_{none | slow} +extendedhint1key

--- src/main/java/mousemaster/ZoomCenter.java
+++ src/main/java/mousemaster/ZoomCenter.java
@@ -1,17 +1,32 @@
 package mousemaster;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public enum ZoomCenter {
 
     SCREEN_CENTER,
     MOUSE,
     LAST_SELECTED_HINT;
 
+    private static final Logger logger = LoggerFactory.getLogger(ZoomCenter.class);
+
     public Point centerPoint(Rectangle screenRectangle, int mouseX,
-                      int mouseY, Point lastSelectedHintPoint) {
+                             int mouseY, Point lastSelectedHintPoint) {
         return switch (this) {
             case SCREEN_CENTER -> screenRectangle.center();
             case MOUSE -> new Point(mouseX, mouseY);
-            case LAST_SELECTED_HINT -> lastSelectedHintPoint;
+            case LAST_SELECTED_HINT -> {
+                if (lastSelectedHintPoint == null) {
+                    logger.warn(
+                            "Cannot use last selected hint as zoom center because " +
+                            "no hint was selected, this is likely due to " +
+                            "a misconfiguration of <previous-mode>.hint.select, " +
+                            "using screen center as fallback");
+                    yield screenRectangle.center();
+                }
+                yield lastSelectedHintPoint;
+            }
         };
     }
 }
